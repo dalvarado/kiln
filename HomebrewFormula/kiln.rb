e47526a5172ad1b38e42246f0e4408457ae8690f
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-05 15:44:56 -0800

    Brew formula update for kiln version 0.44.0

--- HomebrewFormula/kiln.rb
+++ HomebrewFormula/kiln.rb
@@ -2,16 +2,16 @@
 class Kiln < Formula
   desc ""
   homepage ""
-  version "0.42.0"
+  version "0.44.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dalvarado/kiln/releases/download/0.42.0/kiln-darwin-0.42.0.tar.gz"
-    sha256 "2eb20cef906634933acf6253bb682049e2e2ede5609dfe99af7ae82c788ba111"
+    url "https://github.com/dalvarado/kiln/releases/download/0.44.0/kiln-darwin-0.44.0.tar.gz"
+    sha256 "c6d83d86a5c5cdb1d474a1a91615addc28903eb44004faebed929e5742d85dba"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/dalvarado/kiln/releases/download/0.42.0/kiln-linux-0.42.0.tar.gz"
-      sha256 "086945be0647e6bd39cb9642fea27b4db18e4deb5019d6c34adb521adb16dfff"
+      url "https://github.com/dalvarado/kiln/releases/download/0.44.0/kiln-linux-0.44.0.tar.gz"
+      sha256 "01a752112e9928492e1f2f59e971f6026d6196a09166731eefd1710f24bb9c56"
     end
   end
 
